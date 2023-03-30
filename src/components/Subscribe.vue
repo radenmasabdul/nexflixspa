c2eefa49d67e3636d15fa3735da4adcfd4fbb442
radenmasabdul <abdul.tamsis@gmail.com>
2023-03-30 18:27:43 +0700

    subscribe

---
+++ src/components/Subscribe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-[#C8CFEC] shadow-xl mx-10 rounded-lg">
+    <div class="flex justify-center text-center pt-4">
+      <p class="font-Montserrat text-xl">
+        Berlangganan Untuk Mendapatkan Informasi Terbaru dan <br />Menjadi
+        Anggota Kami
+      </p>
+    </div>
+    <div class="flex justify-center gap-4 p-4">
+      <input
+        type="text"
+        placeholder="Enter Your Email"
+        class="input input-bordered input-info w-full max-w-xs"
+      />
+      <button
+        class="btn bg-button-red font-semibold text-base hover:bg-white hover:text-black border-none capitalize"
+      >
+        Berlangganan
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar.vue";
 import Hero from "../components/Hero.vue";
 import Services from "../components/Services.vue";
 import Footer from "../components/Footer.vue";
+import Subscribe from "../components/Subscribe.vue";
 import PacketList from "../components/PacketList.vue";
 </script>
 
@@ -12,6 +13,7 @@ import PacketList from "../components/PacketList.vue";
     <Hero />
     <Services />
     <PacketList />
+    <Subscribe />
     <Footer />
   </main>
 </template>
